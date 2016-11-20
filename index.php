8ebb1bc75d04df486d3936b47930175488a48b68
Ann <sie_moe@mail.ru>
2016-11-20 18:46:27 +0400

    1first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$db = new SQLite3('db.sqlite');
+if (filesize('db.sqlite')==0){
+    $sql= "create table foo(id INTEGER PRIMARY KEY, name, famili)";
+    $db->exec($sql);
+
+}
+
+<form metod >
+
+$name = $_GET($name);
+
+if ($name!==null){
+$sql = "insert into foo VALUES (NULL, '$name', 'test')";
+}
+fdfdfddfdfdf
+echo "hi";
+?>
